4fd36c447aac06b96043ebd0d997f3a284b7dcc6
Ali SABOURI <72344723+alisabourii@users.noreply.github.com>
2025-05-19 20:23:43 +0300

    Update PID_KOD.ino

--- PID_KOD.ino
+++ PID_KOD.ino
@@ -14,10 +14,8 @@ int baseSpeed = 150; // Temel motor hızı
 int maxSpeed = 255;  // Maksimum PWM değeri
 
 void setup() {
-  // Motor pinleri ayarla
   pinMode(3, OUTPUT);  // Sol motor PWM
   pinMode(5, OUTPUT);  // Sağ motor PWM
-
   Serial.begin(9600);
 }
 
@@ -44,10 +42,6 @@ void loop() {
   delay(20); // PID döngü süresi
 }
 
-// Çizgi sensörü verisini simüle eden fonksiyon (yerine gerçek sensör kodu eklenmeli)
 int readLineError() {
-  // Burada çizgi sensörlerinden çizginin konumu hesaplanır
-  // Örnek: analog sensörlerden çizgi pozisyonu hesapla
-  // Bu örnek sabit değer döndürür, gerçek uygulamada sensör okuma kodu eklenecek
   return analogRead(A0) - 512;  // A0 sensör ortalaması varsayımı
 }
